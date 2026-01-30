f4d4d97a883c5e07a9f8c20bef5cf3997eefe384
calvinx-git <calvinx91@gmail.com>
2026-01-30 16:30:48 -0500

    Add files via upload

---
+++ HashComparatorV2.ps1
@@ -0,0 +1,58 @@
+﻿#Calvin's script for comparing hashes V2
+#automatically pulls SHA256 hash values for a file and checks it against another file or manual input
+
+#May need to run following command to allow scripts to run
+#Get-ExecutionPolicy -list
+#Set-ExecutionPolicy RemoteSigned
+#Unblock-File -Path .\HashComparatorV2.ps1
+
+#prompt for first file to check hash
+$dialog = [System.Windows.Forms.OpenFileDialog]::new()
+$dialog.InitialDirectory = $PSScriptRoot
+$result = $dialog.ShowDialog()
+if ($result -eq [System.Windows.Forms.DialogResult]::OK) 
+{
+    $filePath1 = $dialog.FileName
+    $fileHash1 = (Get-FileHash -Path $filePath1 -Algorithm SHA256).Hash
+}
+else
+{
+    Write-Error "File selection cancelled by user."
+    return $null
+}
+
+
+#ask to select a file or hash string to compare
+$result = [System.Windows.Forms.MessageBox]::Show('Select a second file? No for manual hash input', 'Confirmation', 'YesNo')
+if ($result -eq [System.Windows.Forms.DialogResult]::Yes) 
+{
+    $dialog = [System.Windows.Forms.OpenFileDialog]::new()
+    $dialog.InitialDirectory = $PSScriptRoot
+    $result = $dialog.ShowDialog()
+    if ($result -eq [System.Windows.Forms.DialogResult]::OK) 
+    {
+        $filePath2 = $dialog.FileName
+        $fileHash2 = (Get-FileHash -Path $filePath2 -Algorithm SHA256).Hash
+    }
+    else
+    {
+        Write-Error "File selection cancelled by user."
+        return $null
+    }
+} elseif ($result -eq [System.Windows.Forms.DialogResult]::No) 
+{
+    $fileHash2 = Read-Host -Prompt "Input manual Hash info"
+}
+
+#checks if the hash values are equal
+if ($fileHash1 -eq $fileHash2)
+{
+    Echo "The hashes match"
+}
+else
+{
+    Echo "The hashes do NOT match"
+}
+
+#May need to run following command to return security settings to default
+#Set-ExecutionPolicy Undefined
